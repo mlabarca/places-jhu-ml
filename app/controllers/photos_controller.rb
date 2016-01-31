4ab613913dc40d1b97d2715a6a58e04adb3408f1
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-01-31 16:27:38 -0300

    Handle photo not found in controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,8 @@
 class PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
-    send_data @photo.contents, { type: 'image/jpeg', disposition: 'inline'}
+    if @photo.presence
+      send_data @photo.contents, { type: 'image/jpeg', disposition: 'inline'}
+    end
   end
 end
