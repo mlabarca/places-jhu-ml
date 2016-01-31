1a38bf6121f367b2d48f50dc74d5094d7cd546af
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-01-30 22:33:01 -0300

    Add Place Aggregated queries class methods

--- app/models/place.rb
+++ app/models/place.rb
@@ -44,4 +44,50 @@ class Place
   def destroy
     self.class.collection.find(_id: BSON::ObjectId.from_string(@id)).delete_one
   end
+
+  # Gets a list of addresses with locations, returns mongo view
+  def self.get_address_components(sort = {}, offset = 0, limit = nil)
+    fields = %w[_id address_components formatted_address geometry.geolocation]
+    projection = fields.each_with_object({}){|key, p| p[key] = 1}
+    
+    aggregate_query = [
+      {"$unwind" => "$address_components"}, 
+      {"$project" => projection},
+    ]
+    aggregate_query << {"$sort" => sort} if sort.presence 
+    aggregate_query << {"$skip" => offset} if offset.presence 
+    aggregate_query << {"$limit" => limit} if limit.presence 
+    collection.find().aggregate(aggregate_query)
+  end
+  
+  # Fetches a list of available countries in places database, returns array
+  def self.get_country_names
+    fields = %w[address_components.long_name address_components.types]
+    projection = fields.each_with_object({}){|key, p| p[key] = 1}
+    
+    aggregate_query = [
+      {"$project" => projection},
+      {"$unwind"  => "$address_components"},
+      {"$unwind"  => "$address_components.types"},
+      {"$match"   => {"address_components.types" => "country"}},
+      {"$group"   => {"_id" => "$address_components.long_name" }}
+    ]
+    country_docs = collection.find().aggregate(aggregate_query).to_a
+    country_docs.map{|country_doc| country_doc[:_id]}
+  end
+
+  # Finds ids of all places with given country code, returns array
+  def self.find_ids_by_country_code country_code
+    aggregate_query = [
+      {"$match"   => 
+        {"$and" => [
+          "address_components.types"      => {"$in" => ["country"]},
+          "address_components.short_name" => {"$eq" => country_code}
+        ]}
+      },
+      {"$project" => {"_id" => 1}},
+    ]
+    docs = collection.find().aggregate(aggregate_query).to_a
+    docs.map{|doc| doc[:_id].to_s}
+  end
 end
