aa91de52964306531d743c13a85719b528f2d63e
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-01-31 13:34:00 -0300

    Add Place Assoc. to photo and nearest photo method

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo
-  attr_accessor :id, :location
+  attr_accessor :id, :location, :place
   attr_writer :contents
 
   def self.mongo_client
@@ -9,6 +9,7 @@ class Photo
   def initialize params = {}
     @id = params[:_id].to_s if params[:_id]
     @location = Point.new(params[:metadata][:location]) if params[:metadata]
+    @place = params[:metadata][:place] if params[:metadata]
   end
 
   def contents
@@ -26,23 +27,45 @@ class Photo
    !@id.nil?
   end
   
+  def place
+    @place.presence ? Place.find(@place) : nil
+  end
+
+  def place=(place_id)
+    place_id = BSON::ObjectId.from_string(place_id) if place_id.class == String
+    @place = place_id
+  end
+
   # Save jpeg in contents to gridfs in mongo and update instance variables
   def save
-    if !persisted? && @contents
-      gps = EXIFR::JPEG.new(@contents).gps
-      location = Point.new(:lng => gps.longitude, :lat => gps.latitude)
-      
-      @contents.rewind # Reposition read location to beggining of file
-      
-      params = {content_type: "image/jpeg", metadata: {location: location.to_hash}}
-      grid_file = Mongo::Grid::File.new(@contents.read, params) # Unsaved gridfs file
+    if !persisted? 
+      if @contents
+        gps = EXIFR::JPEG.new(@contents).gps
+        @location = Point.new(:lng => gps.longitude, :lat => gps.latitude)
+        
+        @contents.rewind # Reposition read location to beggining of file
+        
+        grid_file = Mongo::Grid::File.new(@contents.read, get_description) # Unsaved gridfs file
 
-      # Store file to mongo db
-      id = self.class.mongo_client.database.fs.insert_one(grid_file)
+        # Store file to mongo db
+        id = self.class.mongo_client.database.fs.insert_one(grid_file)
 
-      @location = location
-      @id = id.to_s
+        @id = id.to_s
+      end
+    else
+      file = self.class.mongo_client.database.fs.find(_id: BSON::ObjectId.from_string(@id))
+      file.update_one(get_description)
     end
+  end
+
+  def get_description
+    description = {content_type: "image/jpeg"}
+    if @location || @place
+      description[:metadata] = {}
+      description[:metadata][:location] = @location.to_hash if @location
+      description[:metadata][:place] = @place
+    end
+    description
   end
 
   def destroy
@@ -60,6 +83,12 @@ class Photo
     result = mongo_client.database.fs.find(_id: BSON::ObjectId.from_string(id)).first
     return result.nil? ? nil : Photo.new(result)
   end
-
+  
+  def find_nearest_place_id max_meters
+    near_places = Place.near(@location, max_meters)
+    nearest_place = near_places.limit(1)
+    nearest_place = nearest_place.projection(_id: 1)
+    nearest_place.find.to_a.first[:_id]
+  end
 
 end
